017b8af9cbcb0b7be8f8901e6b11beb39dd9b398
simay5349 <yildirim.simay.duru@ibu.edu.mk>
2026-03-11 22:48:13 +0000

    finished phonebook1.c

--- memory-lab/04-file-io/phonebook1.c
+++ memory-lab/04-file-io/phonebook1.c
@@ -3,30 +3,33 @@
 // This is the safe version — always check that fopen succeeded
 // before writing to the file.
 
-#include <cs50.h>
+
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
     // TODO: Open "phonebook.csv" in append mode "a"
-    FILE *file = fopen(/* ??? */, /* ??? */);
+    FILE *file = fopen("phonebook.csv" , "a");
 
     // TODO: Check if fopen returned NULL and return 1 if so
-    if (/* ??? */)
+    if (file == NULL)
     {
         return 1;
     }
 
     // Get name and number from the user
-    char *name = get_string("Name: ");
-    char *number = get_string("Number: ");
-
+    char name[100];
+    char number[100];
+    printf("Name: ");
+    scanf("%99s" , name);
+    printf("Number: ");
+    scanf("%99s" , number);
     // TODO: Write name and number to the file as "name,number\n"
-    fprintf(file, /* ??? */, name, number);
+    fprintf(file, "%s,%s\n", name, number);
 
     // TODO: Close the file
-    /* ??? */
+    fclose(file);
 
     return 0;
 }
